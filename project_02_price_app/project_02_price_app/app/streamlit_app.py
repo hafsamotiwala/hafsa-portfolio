07b7c8c4e25d786f9867fd7147888a1d5d33c34d
Hafsa <motiwala603@gmail.com>
2025-12-31 21:07:14 +0500

    Restore app folder with streamlit_app.py and model

--- project_02_price_app/project_02_price_app/app/streamlit_app.py
+++ project_02_price_app/project_02_price_app/app/streamlit_app.py
@@ -3,9 +3,9 @@ import pandas as pd
 import numpy as np
 import joblib
 
-# -------------------------------
+# ------------------------------
 # Page Configuration
-# -------------------------------
+# ------------------------------
 st.set_page_config(
     page_title="House Price Estimator",
     layout="centered",
